028114bdd24058a7b7b4aeea277283bb5221e8dc
Manas-kmcdu <manaspandeya@gmail.com>
2026-05-20 16:56:42 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -16,6 +16,8 @@ const signInWithPopup = (...a) => fb().signInWithPopup(...a);
 const signOut       = (...a) => fb().signOut(...a);
 const onAuthStateChanged = (...a) => fb().onAuthStateChanged(...a);
 
+const CSS = `
+  @import url('https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,400&display=swap');
   *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
   :root {
     --paper: #f2efe8;
